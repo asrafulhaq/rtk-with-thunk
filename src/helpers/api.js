e6dc2f66ab02b206591d472ba04d08e10738215f
asrafulhaq <devvorb@gmail.com>
2025-01-15 21:30:31 +0600

    Upload team files

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // create API
 const API = axios.create({
-  baseURL: "http://localhost:5050",
+  baseURL: "https://devzone-rest-apis.onrender.com",
   timeout: "20000",
   withCredentials: false,
 });
